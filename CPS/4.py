0b3d982d6d1d334c2aad0d9aaeae6de53f7046f1
PARDHIV VARMA <118751512+codinggeek1@users.noreply.github.com>
2023-04-16 23:00:39 +0530

    Create 4.py

---
+++ CPS/4.py
@@ -0,0 +1,28 @@
+import time 
+import requests 
+import Adafruitat_DHT
+
+apikey = "......."
+int pin = 4
+int sensor = Adafruit_DHT.DHT11
+humid = [] 
+temp = []
+
+for i in range(5):
+  h,t = Adafruit_DHT.read(sensor,pin)
+  humid.append(h)
+  temp.append(t)
+  time.sleep(5)
+  
+for i in range(5):
+  t = temp[i]
+  h = humid[i]
+  payload = { "API" : apikey, "field" : str(t), "field2" : str(h) }
+  req = requests.post(".....", params = payload)
+  time.sleep(5)
+  
+if(req ok):
+  printf("Updated")
+
+else:
+  printf("Error")
